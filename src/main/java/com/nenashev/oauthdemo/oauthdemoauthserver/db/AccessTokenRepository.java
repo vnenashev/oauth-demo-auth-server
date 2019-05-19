1f557edbdb907b2f163fc719e21da412c1f9929f
Vladimir Nenashev <vn@payneteasy.com>
2019-05-19 14:58:22 +0300

    fixed test config for DB

--- src/main/java/com/nenashev/oauthdemo/oauthdemoauthserver/db/AccessTokenRepository.java
+++ src/main/java/com/nenashev/oauthdemo/oauthdemoauthserver/db/AccessTokenRepository.java
@@ -2,7 +2,9 @@ package com.nenashev.oauthdemo.oauthdemoauthserver.db;
 
 import com.nenashev.oauthdemo.oauthdemoauthserver.model.AccessTokenInfo;
 
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+@Profile("!test")
 public interface AccessTokenRepository extends MongoRepository<AccessTokenInfo, String> {
 }
--- src/test/java/com/nenashev/oauthdemo/oauthdemoauthserver/OauthDemoAuthServerApplicationTests.java
+++ src/test/java/com/nenashev/oauthdemo/oauthdemoauthserver/OauthDemoAuthServerApplicationTests.java
@@ -1,16 +1,23 @@
 package com.nenashev.oauthdemo.oauthdemoauthserver;
 
+import com.nenashev.oauthdemo.oauthdemoauthserver.db.AccessTokenRepository;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@ActiveProfiles("test")
 public class OauthDemoAuthServerApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @MockBean
+    private AccessTokenRepository accessTokenRepository;
 
+    @Test
+    public void contextLoads() {
+    }
 }
