d8369786066c4efa21a3e0c5fcd930cee9f5f114
Vladimir Nenashev <vn@payneteasy.com>
2019-06-01 11:21:49 +0300

    fixed test context

--- src/main/java/com/nenashev/oauthdemo/oauthdemoauthserver/scheduler/AccessTokenExpireScheduler.java
+++ src/main/java/com/nenashev/oauthdemo/oauthdemoauthserver/scheduler/AccessTokenExpireScheduler.java
@@ -10,10 +10,12 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Profile;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Component
+@Profile("!test")
 public class AccessTokenExpireScheduler {
 
     private final Logger logger = LoggerFactory.getLogger(AccessTokenExpireScheduler.class);
